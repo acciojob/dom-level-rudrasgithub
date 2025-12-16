e6062f4472d7161037658a091800c1d891284a0b
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-16 22:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let ele = document.getElementById("level");
+let depth = 0;
+while (ele && ele.parentNode && ele.parenNode !== document) {
+	ele = ele.parentNode;
+	depth++;
+}
+alert(depth)
